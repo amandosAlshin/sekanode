042da6e20ce2ebc68dbeaaddf82dfe4f8ddba4a9
mac <mac@TTs-MacBook-Pro.local>
2021-03-10 01:06:54 +0600

    mysql connect build

--- dist/services/connectionAsmazirDB.js
+++ dist/services/connectionAsmazirDB.js
@@ -1,30 +1,34 @@
 "use strict";
 
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
 
+var _mysql = _interopRequireDefault(require("mysql"));
+
 var _vars = require("../config/vars.js");
 
-var mariadb = require('mariadb/callback');
+var util = require('util');
 
-var _require = require('util'),
-    promisify = _require.promisify;
-
-var db = mariadb.createConnection({
+var db = _mysql["default"].createConnection({
   host: _vars.asmazirDB.host,
   user: _vars.asmazirDB.user,
-  database: _vars.asmazirDB.database,
-  password: _vars.asmazirDB.password
-});
+  password: _vars.asmazirDB.password,
+  database: _vars.asmazirDB.database
+}); // connect to database
+
+
 db.connect(function (err) {
   if (err) {
-    console.log("not connected due to error: " + err);
-  } else {
-    console.log("connected ! connection id is " + conn.threadId);
+    console.log('error mysql connect', err);
+    return err;
   }
+
+  console.log('asmazir db connected');
 });
-db.query = promisify(db.query);
+db.query = util.promisify(db.query);
 var _default = db;
 exports["default"] = _default;
