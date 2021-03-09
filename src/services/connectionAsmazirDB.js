d177aff2565af128e87b89ebf3c27e343e58ad1b
mac <mac@TTs-MacBook-Pro.local>
2021-03-10 01:06:19 +0600

    mysql connect

--- src/services/connectionAsmazirDB.js
+++ src/services/connectionAsmazirDB.js
@@ -1,20 +1,20 @@
-const mariadb = require('mariadb/callback');
-const { promisify } = require('util')
+import mysql from 'mysql';
+const util = require('util');
 import {asmazirDB} from '../config/vars.js';
-
-
-const db = mariadb.createConnection({
+const db = mysql.createConnection({
     host: asmazirDB.host,
-    user:   asmazirDB.user,
-    database: asmazirDB.database,
-    password: asmazirDB.password,
-    });
-db.connect(err => {
-  if (err) {
-    console.log("not connected due to error: " + err);
-  } else {
-    console.log("connected ! connection id is " + conn.threadId);
-  }
+    user:  asmazirDB.user,
+    password:  asmazirDB.password,
+    database:  asmazirDB.database,
 });
-db.query = promisify(db.query);
+
+// connect to database
+db.connect((err) => {
+    if (err) {
+        console.log('error mysql connect',err);
+        return err;
+    }
+    console.log('asmazir db connected');
+});
+db.query = util.promisify(db.query);
 export default db;
